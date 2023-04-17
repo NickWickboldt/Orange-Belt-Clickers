14494c3cce3d9358cd7baf76ddeb85a662bd6282
NickWickboldt <113066606+NickWickboldt@users.noreply.github.com>
2023-04-17 17:24:06 -0500

    Add files via upload

---
+++ lists.js
@@ -0,0 +1,51 @@
+export let blockArray = [
+    "./items/dirt.png",
+    "./items/stone.png",
+    "./items/coal_ore.png",
+    "./items/iron_ore.png",
+    "./items/lapis_ore.png",
+    "./items/gold_ore.png",
+    "./items/redstone_ore.png",
+    "./items/diamond_ore.png",
+    "./items/emerald_ore.png",
+    "./items/obsidian.png",
+    "./items/sculk.png",
+    "./items/netherrack.png",
+    "./items/magma_block.png",
+    "./items/nether_quartz_ore.png",
+    "./items/nether_gold_ore.png",
+    "./items/ancient_debris.png",
+    "./items/end_stone.png",
+    "./items/purpur_block.png",
+    "./items/bedrock.png"
+];
+
+export let pickaxeArray = [
+    "./items/wooden_pickaxe.png",
+    "./items/stone_pickaxe.png",
+    "./items/iron_pickaxe.png",
+    "./items/lapis_pickaxe.png",
+    "./items/golden_pickaxe.png",
+    "./items/redstone_pickaxe.png",
+    "./items/diamond_pickaxe.png",
+    "./items/emerald_pickaxe.png",
+    "./items/obsidian_pickaxe.png",
+    "./items/netherack_pickaxe.png",
+    "./items/quartz_pickaxe.png",
+    "./items/nether_brick_pickaxe.png",
+    "./items/blaze_pickaxe.png",
+    "./items/wither_bone_pickaxe",
+    "./items/netherite_pickaxe",
+    "./items/end_stone_pickaxe.png",
+    "./items/ender_pearl_pickaxe.png",
+    "./items/end_crystal_pickaxe.png",
+    "./items/dragon_pickaxe.png",
+    "./items/shulker_pickaxe.png",
+    "./items/bedrock_pickaxe.png"
+];
+
+export let dimensionList = [
+    "./dimensions/overworld.jpeg",
+    "./dimensions/nether.png",
+    "./dimensions/end.avif",
+];
