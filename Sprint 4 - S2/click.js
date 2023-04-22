459128576cff217f141f07b72c8d8275f95e2d57
NickWickboldt <113066606+NickWickboldt@users.noreply.github.com>
2023-04-22 11:06:54 -0500

    Add files via upload

--- Sprint 4 - S2/click.js
+++ Sprint 4 - S2/click.js
@@ -1,5 +1,6 @@
 const nick = document.querySelector(".nick");
 const berryLabel = document.querySelector(".berries");
+const autoClickerButton = document.querySelector(".auto-upgrade");
 
 let strawberries = 0;
 
@@ -15,3 +16,19 @@ nick.addEventListener("click",()=>{
     strawberries = strawberries + 1;
     berryLabel.innerHTML = "Strawberries: " + strawberries;
 });
+
+let autoClickerPrice = 5000;
+autoClickerButton.addEventListener("click",()=>{
+    if(strawberries>=autoClickerPrice){
+        strawberries-=autoClickerPrice;
+        berryLabel.innerHTML = "Strawberries: " + strawberries;
+        autoClickerActivate();
+    }
+});
+
+function autoClickerActivate(){
+    setInterval(() => {
+        strawberries++;
+        berryLabel.innerHTML = "Strawberries: " + strawberries;
+    }, 1000);
+}
